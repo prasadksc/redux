d11a1521e9e85f7dda54a04ae03b5f2e2e7214b1
prasadksc <prasadksc123@gmail.com>
2021-11-12 11:40:56 +0530

    first commit

---
+++ src/reducers/isLogged.js
@@ -0,0 +1,13 @@
+
+
+const loggedReducer = (state = false,action) => {
+    switch (action.type) {
+        case 'SIGNIN':
+            return !state
+        default:
+            return state
+    }
+}
+
+
+export default loggedReducer;
